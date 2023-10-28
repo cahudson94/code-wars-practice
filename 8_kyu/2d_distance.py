2adbafd4aa38d7e3467076a193f9aeebd677232a
Chris Hudson <c.ahudson84@yahoo.com>
2023-10-27 19:49:36 -0700

    Day two.

---
+++ 8_kyu/2d_distance.py
@@ -0,0 +1,13 @@
+"""
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have attributes x and y.
+
+Write a function calculating distance between Point a and Point b.
+
+Input coordinates fit in range −50⩽x,y⩽50. Tests compare expected result and actual answer with tolerance of 1e-6.
+"""
+
+def distance_between_points(a, b):
+	"""Use pythagorean theorum to find distance between two points."""
+    return (abs(a.x - b.x) ** 2 + abs(a.y - b.y) ** 2) ** 0.5
