a08f6a4aa487d3b14449035ef5ce6c9c6246f920
Chris Hudson <c.ahudson84@yahoo.com>
2023-12-10 12:00:00 -0800

    Day 44

---
+++ 7_kyu/dec_23/centroid_1.py
@@ -0,0 +1,8 @@
+"""
+Write a function centroid(c) to calculate the centroid of 3D coordinates.
+
+Return the results as a list of floats. Round the values to two decimal places.
+"""
+
+def centroid(c):
+    return [round(sum(x) / len(c), 2) for x in zip(*c)]
