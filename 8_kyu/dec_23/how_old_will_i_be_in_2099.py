921c933e3143ce4157b853daffb56653cc2a5398
Chris Hudson <c.ahudson84@yahoo.com>
2023-12-22 20:04:20 -0800

    Day 56

---
+++ 8_kyu/dec_23/how_old_will_i_be_in_2099.py
@@ -0,0 +1,17 @@
+"""
+Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+
+Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+
+Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+
+"..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.
+
+Good Luck!
+"""
+
+def calculate_age(year_of_birth, current_year):
+    year_diff = current_year - year_of_birth
+    more_than_a_year = f"You are {year_diff} year{'s' if year_diff > 1 else ''} old."
+    not_born = f"You will be born in {abs(year_diff)} year{'s' if year_diff < -1 else ''}."
+    return more_than_a_year if year_diff > 0 else not_born if year_diff < 0 else "You were born this very year!"
