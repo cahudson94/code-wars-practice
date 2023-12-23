d9cb9392513e45e4d6f91f366d6cec265fc424e8
Chris Hudson <c.ahudson84@yahoo.com>
2023-12-23 12:00:00 -0800

    Day 58

---
+++ 7_kyu/dec_23/find_missing_numbers.py
@@ -0,0 +1,16 @@
+"""
+You will get an array of numbers.
+
+Every preceding number is smaller than the one following it.
+
+Some numbers will be missing, for instance:
+
+[-3,-2,1,5] //missing numbers are: -1,0,2,3,4
+
+Your task is to return an array of those missing numbers:
+
+[-1,0,2,3,4]
+"""
+
+def find_missing_numbers(arr):
+    return [y for y in range(arr[0], arr[-1]) if y not in arr] if arr else []
