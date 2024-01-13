84e99803ae794b331dacc015f2265b8e055c21f0
Chris Hudson <c.ahudson84@yahoo.com>
2024-01-13 12:00:00 -0800

    Day 79

---
+++ 7_kyu/2024/jan/sort_and_transform.py
@@ -0,0 +1,26 @@
+"""
+Given an array of numbers, return a string made up of four parts:
+
+    a four character 'word', made up of the characters derived from the first two and last two numbers in the array. order should be as read left to right (first, second, second last, last),
+
+    the same as above, post sorting the array into ascending order,
+
+    the same as above, post sorting the array into descending order,
+
+    the same as above, post converting the array into ASCII characters and sorting alphabetically.
+
+The four parts should form a single string, each part separated by a hyphen (-).
+
+Example format of solution: "asdf-tyui-ujng-wedg"
+Examples
+
+[111, 112, 113, 114, 115, 113, 114, 110]  -->  "oprn-nors-sron-nors"
+[66, 101, 55, 111, 113]                   -->  "Beoq-7Boq-qoB7-7Boq"
+[99, 98, 97, 96, 81, 82, 82]              -->  "cbRR-QRbc-cbRQ-QRbc"
+"""
+
+def get_chars(arr):
+    return ''.join(map(chr, arr[:2] + arr[-2:]))
+
+def sort_transform(arr):
+    return f"{get_chars(arr)}-{get_chars(sorted(arr))}-{get_chars(sorted(arr, reverse=True))}-{get_chars(sorted(arr))}"
