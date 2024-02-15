7aeddf0b2edac260dced9fb0f039c28a8cf93dc6
Chris Hudson <c.ahudson84@yahoo.com>
2024-02-14 17:25:04 -0800

    Day 111

---
+++ 6_kyu/2024/feb/array_prod_sans_n.py
@@ -0,0 +1,38 @@
+"""
+Related to MrZizoScream's Product Array kata. You might want to solve that one first :)
+
+This is an adaptation of a problem I came across on LeetCode.
+
+Given an array of numbers, your task is to return a new array where each index (new_array[i]) is equal to the product of the original array, except for the number at that index (array[i]).
+
+Two things to keep in mind:
+
+    Zeroes will be making their way into some of the arrays you are given
+    O(n^2) solutions will not pass.
+
+Examples:
+
+product_sans_n([1,2,3,4]) => [24, 12, 8, 6]
+product_sans_n([2,3,4,5]) => [60, 40, 30, 24]
+product_sans_n([1,1,1]) => [1, 1, 1]
+product_sans_n([9,0,-2]) => [0, -18, 0])
+product_sans_n([0,-99,0]) => [0, 0, 0])
+product_sans_n([3,14,9,11,11]) => [15246, 3267, 5082, 4158, 4158])
+product_sans_n([-8,1,5,13,-1]) => [-65, 520, 104, 40, -520])
+product_sans_n([4,7,3,6,2,14,7,5]) => [123480, 70560, 164640, 82320, 246960, 35280, 70560, 98784]
+
+Note: All inputs will be valid arrays of nonzero length.
+
+Have fun! Please upvote if you enjoyed :)
+"""
+
+from math import prod
+
+def product_sans_n(nums):
+    if nums.count(0) > 1:
+        return [0 for x in nums]
+    if 0 in nums:
+        base = prod([x if x else 1 for x in nums])
+        return [0 if x else base for x in nums]
+    base = prod(nums)
+    return [base // x for x in nums]
