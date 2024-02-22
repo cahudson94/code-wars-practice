e52508450d626561459e63a17ca3fa0a2f645da3
Chris Hudson <c.ahudson84@yahoo.com>
2024-02-22 12:00:00 -0800

    Day 119

---
+++ 7_kyu/2024/feb/string_reversing_chaging_case_etc.py
@@ -0,0 +1,12 @@
+"""
+Given 2 string parameters, show a concatenation of:
+
+    the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
+    a separator in between both strings: @@@
+    the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER 
+
+** Keep in mind that this kata was initially designed for Shell, I'm aware it may be easier in other languages.**
+"""
+
+def reverse_and_mirror(s1, s2):
+    return f"{s2[::-1].swapcase()}@@@{s1[::-1].swapcase() + s1.swapcase()}"
